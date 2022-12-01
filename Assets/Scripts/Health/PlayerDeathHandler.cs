e58a83eadcd2479f09c98f424335d594766752af
Alex Bates <alex@nanaian.town>
2022-12-01 19:55:25 +0000

    fix death

--- Assets/Scripts/Health/PlayerDeathHandler.cs
+++ Assets/Scripts/Health/PlayerDeathHandler.cs
@@ -1,10 +1,14 @@
 using Effects;
 using UnityEngine;
+using World;
 
 namespace Health {
     public class PlayerDeathHandler : DeathHandler {
         public override void OnDeath() {
-            Debug.Log("Player died!");
+            // See also DeathUi
+
+            // Pause time
+            TimeEffect.Spawn(1000f, 0.2f);
         }
     }
 }
--- Assets/Scripts/UI/DeathUi.cs
+++ Assets/Scripts/UI/DeathUi.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Ui {
     public class DeathUi : MonoBehaviour {
@@ -6,6 +7,8 @@ namespace Ui {
         public GameObject world;
         public GameObject deathScreen;
         public AudioSource audioToStop;
+
+        private bool triggered;
 
         private void Start() {
             deathScreen.SetActive(false);
@@ -17,7 +20,17 @@ namespace Ui {
                 //world.SetActive(false);
                 deathScreen.SetActive(true);
                 audioToStop.volume = Mathf.Lerp(audioToStop.volume, 0, Time.deltaTime * 4f);
+
+                // hotfix due to death menu not working
+                if (!triggered) {
+                    triggered = true;
+                    Invoke(nameof(BackToVillage), 1f);
+                }
             }
+        }
+
+        private void BackToVillage() {
+            SceneManager.LoadScene("VillageScene");
         }
     }
 }
